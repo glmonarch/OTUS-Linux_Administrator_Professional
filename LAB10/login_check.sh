2578406c846607f8aad2363fc031ead60c6532b0
glmonarch <glmonarch@users.noreply.github.com>
2019-09-16 11:55:36 +0300

    Create login_check.sh

---
+++ LAB10/login_check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create file with permited users:
+lid -g admin | awk 'BEGIN{FS="("} {print $1}' > /etc/pam.d/pam_permited_users.txt
+
+# �ompare $PAM_USER with permited users:
+if grep -w $PAM_USER /etc/pam.d/pam_permited_users.txt
+then
+	exit 0
+else
+# Compare current week day with Saturday & Sunday:
+	if [ $(date +%a) = "Sat" ]
+	then
+		exit 1
+	elif [ $(date +%a) = "Sun" ]
+	then
+		exit 1
+	else
+		exit 0
+
+	fi
+fi
